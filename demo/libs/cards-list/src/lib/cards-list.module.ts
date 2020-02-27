2be004c7bf8b4b682b56fc46f65e2809a7d27f1d
Farooq Mahmud <farooq@teqniqly.com>
2020-02-26 17:16:49 -0800

    list component

--- demo/libs/cards-list/src/lib/cards-list.module.ts
+++ demo/libs/cards-list/src/lib/cards-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardsComponent } from './cards/cards.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CardsComponent],
-  exports: [CardsComponent]
+  declarations: [CardsComponent, ListComponent],
+  exports: [CardsComponent, ListComponent]
 })
 export class CardsListModule {}
